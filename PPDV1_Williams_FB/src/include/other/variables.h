1205bef040ebcf7fa88acefb6980a212640ce4fb
Facundo Williams <facuwilli@gmail.com>
2021-10-07 10:47:06 -0300

    Add a scenes enum to store the different scenes

--- PPDV1_Williams_FB/src/include/other/variables.h
+++ PPDV1_Williams_FB/src/include/other/variables.h
@@ -9,4 +9,8 @@ const int screenHeight = 450;
 const short fps = 60;
 static const char* tittle = "Flappy Bird";
 
+//Scenes
+enum class Scenes {menu, credits, gameplay};
+const Scenes initialScene = Scenes::menu;
+
 #endif
